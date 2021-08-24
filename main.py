61d470796a576d20a42a2f3e81115dd39ccb3772
Arthur Dvorkin <arthurd@onenav.ai>
2021-08-24 09:19:31 -1000

    Hi #57

--- main.py
+++ main.py
@@ -6,7 +6,18 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
+    z=0
+    x=10
     print("hello world")  # Press ⌘F8 to toggle the breakpoint.
+    print("hello earth")
+    print("hello space")
+    print("hello Scott")
+    print("hello jim")
+    print("Hi Dave")
+
+    for x in range(0,100):
+        z+=1
+        x+=z
 
 
 # Press the green button in the gutter to run the script.
